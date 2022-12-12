f37ee27aeae082364876d89f7cb0b5f6f1f4c02e
Ferdinand Prantl <prantlf@gmail.com>
2022-12-12 15:12:07 +0100

    fix: Normalise backslashes to forward slashes in the source map paths

--- test/index.js
+++ test/index.js
@@ -17,6 +17,30 @@ test('rebases by prefixes', () => {
   strictEqual(rebase('page/index.js'), 'page/index.js')
 })
 
+test('rebases with backslashes in the configuration', () => {
+  const rebase = createPathTransform({
+    prefixes: {
+      '*src\\components\\': '/compi/comps/',
+      'node_modules\\': '/compi/deps/'
+    }
+  })
+  strictEqual(rebase('/src/components/index.js'), '/compi/comps/index.js')
+  strictEqual(rebase('node_modules/lit/index.js'), '/compi/deps/lit/index.js')
+  strictEqual(rebase('page/index.js'), 'page/index.js')
+})
+
+test('rebases with backslashes in the file paths', () => {
+  const rebase = createPathTransform({
+    prefixes: {
+      '*src/components/': '/compi/comps/',
+      'node_modules/': '/compi/deps/'
+    }
+  })
+  strictEqual(rebase('\\src\\components\\index.js'), '/compi/comps/index.js')
+  strictEqual(rebase('node_modules\\lit\\index.js'), '/compi/deps/lit/index.js')
+  strictEqual(rebase('page\\index.js'), 'page/index.js')
+})
+
 test('reports unrecognised path', () => {
   const rebase = createPathTransform({ requirePrefix: true })
   try {
